df606a410601d681821dcb2fff3493e4d53bf8f5
WANG HONGYUAN <hongyuan.wang@student.uts.edu.au>
2025-05-07 13:20:49 +1000

    Update CorsConfig.java

--- src/main/java/com/HongyuanWang/learningforum/config/CorsConfig.java
+++ src/main/java/com/HongyuanWang/learningforum/config/CorsConfig.java
@@ -21,6 +21,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 // 放行哪些域名（必须用 patterns，否则 * 会和 allowCredentials 冲突）
                 .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:3000", "http://chemxnexus.com.cn")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*");
