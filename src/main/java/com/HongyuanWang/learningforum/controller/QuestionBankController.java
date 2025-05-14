6e5da3ce0baa6adcd29a478de183300c5ddf31b2
WANG HONGYUAN <hongyuan.wang@student.uts.edu.au>
2025-05-14 17:36:46 +1000

    Update QuestionBankController.java

--- src/main/java/com/HongyuanWang/learningforum/controller/QuestionBankController.java
+++ src/main/java/com/HongyuanWang/learningforum/controller/QuestionBankController.java
@@ -180,8 +180,6 @@ public class QuestionBankController {
         // 设置本地缓存（如果不是热 key，这个方法不会设置缓存）
         JdHotKeyStore.smartSet(key, questionBankVO);
 
-
-
         // 获取封装类
         return ResultUtils.success(questionBankVO);
     }
